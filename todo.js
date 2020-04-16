70a17e4a12b0d96649261d40dc6f6de6c36cd7a1
Jiyong95 <49055628+Jiyong95@users.noreply.github.com>
2020-04-16 20:05:43 +0900

    todolist(3)
    todo를 html과 local storage에서 지우기

--- todo.js
+++ todo.js
@@ -4,8 +4,32 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 
-const toDos = [];
+let toDos = [];
 // todo들을 저장할 배열 생성.
+
+function deleteToDo(event) {
+  const btn = event.target;
+  // target은 클릭한 요소<button>❌</button>를 dom으로 반환.
+  const li = btn.parentNode;
+  // btn의 부모 노드
+  toDoList.removeChild(li);
+  // 여기까지가 HTML상에서만 TODO를 지움.
+  // local storage에는 남아있음.
+  // 즉, li만 지워짐.
+  const cleanToDos = toDos.filter(function (toDo) {
+    return toDo.id !== parseInt(li.id);
+    // toDo.id는 int, li.id는 string임.
+    /* 위에서 li가 지워져 toDo.id는 있지만 li.id는 사라진상태.
+     따라서, 위 조건을 통해 toDo.id에서 지워야할 id를 찾음.*/
+    /* filter()는 toDos각각의 원소들에 대해 해당 함수가 실행되고,
+      해당 함수를 만족하는 원소들로만 새로운 배열을 생성.*/
+  });
+  toDos = cleanToDos;
+  // toDos가 const면 값을 변경 불가.
+  // 따라서 let으로 변경.
+  saveToDos();
+}
+
 function paintToDo(text) {
   const li = document.createElement("li");
   const delBtn = document.createElement("button");
@@ -14,6 +38,7 @@ function paintToDo(text) {
   // toDos배열에 아무것도 없을 경우 id를 1로 해주려고.
   // 즉, id를 0이 아닌 1부터 쓰려고.
   delBtn.innerText = "❌";
+  delBtn.addEventListener("click", deleteToDo);
   span.innerText = text;
   li.appendChild(delBtn);
   // li태그 안에 delBtn이 들어감.
